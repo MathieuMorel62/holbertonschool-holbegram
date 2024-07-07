ff400bf57c2400fe6c70b3bd8b576e478034301d
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-07-07 21:24:50 +0200

    chore: Update TextFieldInput widget with filled background color

--- lib/widgets/text_field.dart
+++ lib/widgets/text_field.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+
+// Classe représentant un champ de texte
 class TextFieldInput extends StatelessWidget {
   final TextEditingController controller;
   final bool isPassword;
@@ -7,6 +9,7 @@ class TextFieldInput extends StatelessWidget {
   final Widget? suffixIcon;
   final TextInputType keyboardType;
 
+  // Constructeur pour initialiser les propriétés
   const TextFieldInput({
     super.key,
     required this.controller,
@@ -16,6 +19,7 @@ class TextFieldInput extends StatelessWidget {
     this.keyboardType = TextInputType.text,
   });
 
+  // Méthode pour construire le widget
   @override
   Widget build(BuildContext context) {
     return TextField(
@@ -34,6 +38,7 @@ class TextFieldInput extends StatelessWidget {
           borderSide: BorderSide(color: Colors.transparent),
         ),
         filled: true,
+        fillColor: const Color.fromRGBO(240, 240, 240, 1.0),
         contentPadding: const EdgeInsets.all(8),
         suffixIcon: suffixIcon,
       ),
