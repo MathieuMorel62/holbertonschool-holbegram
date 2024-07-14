97f09d5e565fb6b9001979d7ab3a581f9079bf55
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-07-14 13:00:47 +0200

    Add authentication methods for login and sign-up in auth_methods.dart with Firebase integration.

---
+++ lib/methods/auth_methods.dart
@@ -0,0 +1,66 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:holbegram/models/user.dart';
+import 'dart:typed_data';
+
+class AuthMethods {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  // Méthode pour se connecter
+  Future<String> login({
+    required String email,
+    required String password,
+  }) async {
+    if (email.isEmpty || password.isEmpty) {
+      return 'Please fill all the fields';
+    }
+
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+      return 'success';
+    } catch (e) {
+      return e.toString();
+    }
+  }
+
+  // Méthode pour s'inscrire
+  Future<String> signUpUser({
+    required String email,
+    required String password,
+    required String username,
+    Uint8List? file,
+  }) async {
+    if (email.isEmpty || password.isEmpty || username.isEmpty) {
+      return 'Please fill all the fields';
+    }
+
+    try {
+      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+
+      User? user = userCredential.user;
+
+      if (user != null) {
+        Users newUser = Users(
+          uid: user.uid,
+          email: email,
+          username: username,
+          bio: '',
+          photoUrl: '',
+          followers: [],
+          following: [],
+          posts: [],
+          saved: [],
+          searchKey: username[0].toUpperCase(),
+        );
+
+        await _firestore.collection('users').doc(user.uid).set(newUser.toJson());
+      }
+
+      return 'success';
+    } catch (e) {
+      return e.toString();
+    }
+  }
+}
