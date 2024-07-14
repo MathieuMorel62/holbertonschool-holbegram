a80edd63069b449cfbc7c22234d2b7321247e110
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-07-14 20:43:40 +0200

    Add getUserDetails method to retrieve current user details from Firestore.

--- lib/methods/auth_methods.dart
+++ lib/methods/auth_methods.dart
@@ -3,6 +3,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:holbegram/models/user.dart';
 import 'dart:typed_data';
 
+
+// Classe pour gérer les méthodes d'authentification
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
@@ -16,6 +18,7 @@ class AuthMethods {
       return 'Please fill all the fields';
     }
 
+    // Connexion de l'utilisateur
     try {
       await _auth.signInWithEmailAndPassword(email: email, password: password);
       return 'success';
@@ -35,12 +38,15 @@ class AuthMethods {
       return 'Please fill all the fields';
     }
 
+    // Inscription de l'utilisateur
     try {
       UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
 
+      // Récupérer l'utilisateur actuel
       User? user = userCredential.user;
 
+      // Enregistrer les données de l'utilisateur dans Firestore
       if (user != null) {
         Users newUser = Users(
           uid: user.uid,
@@ -62,5 +68,19 @@ class AuthMethods {
     } catch (e) {
       return e.toString();
     }
+  }
+
+  // Méthode pour obtenir les détails de l'utilisateur actuel
+  Future<Users> getUserDetails() async {
+    User? currentUser = _auth.currentUser;
+
+    // Vérifier si l'utilisateur est connecté
+    if (currentUser == null) {
+      throw Exception('No user logged in');
+    }
+
+    // Récupérer les données de l'utilisateur actuel depuis Firestore
+    DocumentSnapshot snap = await _firestore.collection('users').doc(currentUser.uid).get();
+    return Users.fromSnap(snap);
   }
 }
