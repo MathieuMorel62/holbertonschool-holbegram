f1b591a02385e895f26af7b319cc049830229efd
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-07-18 15:49:44 +0200

    Refactor uploadImageToStorage method in user_storage.dart

--- lib/screens/auth/methods/user_storage.dart
+++ lib/screens/auth/methods/user_storage.dart
@@ -3,20 +3,16 @@ import 'package:uuid/uuid.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
-
-// Classe pour gérer les méthodes de stockage
 class StorageMethods {
   final FirebaseStorage _storage = FirebaseStorage.instance;
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  // Méthode pour télécharger une image
   Future<String> uploadImageToStorage(
-      bool isPost,
+    bool isPost,
     String childName,
     Uint8List file,
-
   ) async {
-    Reference ref =_storage.ref().child(childName).child(_auth.currentUser!.uid);
+    Reference ref = _storage.ref().child(childName).child(_auth.currentUser!.uid);
     if (isPost) {
       String id = const Uuid().v1();
       ref = ref.child(id);
