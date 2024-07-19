1a60332117af150ea9f2534cd7d7cd3b2ac37689
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-07-19 18:59:01 +0200

    Add profile page UI

--- lib/screens/Pages/profile_screen.dart
+++ lib/screens/Pages/profile_screen.dart
@@ -1,20 +1,228 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:holbegram/screens/login_screen.dart';
 
-// Classe pour gérer le profil de l'utilisateur
-class ProfileScreen extends StatelessWidget {
+// Classe qui permet d'afficher le profil de l'utilisateur
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
 
   @override
+  ProfileScreenState createState() => ProfileScreenState();
+}
+
+class ProfileScreenState extends State<ProfileScreen> {
+  var userData = {};
+  int postLen = 0;
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  // Fonction qui permet de récupérer les données de l'utilisateur
+  getData() async {
+    setState(() {
+      isLoading = true;
+    });
+    try {
+
+      // Récupérer les données de l'utilisateur
+      var userSnap = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(FirebaseAuth.instance.currentUser!.uid)
+          .get();
+      userData = userSnap.data()!;
+
+      // Récupérer le nombre de posts de l'utilisateur
+      var postSnap = await FirebaseFirestore.instance
+          .collection('posts')
+          .where('uid', isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+          .get();
+      postLen = postSnap.docs.length;
+      setState(() {});
+    } catch (e) {
+      // ignore: avoid_print
+      print(e.toString());
+    }
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: const Text('Profile'),
-      ),
-      body: const Center(
-        child: Text('Profile Page'),
-      ),
+    return isLoading
+        ? const Center(
+            child: CircularProgressIndicator(),
+          )
+        : Scaffold(
+            backgroundColor: Colors.white,
+            appBar: PreferredSize(
+              preferredSize: const Size.fromHeight(90.0),
+              child: Column(
+                children: [
+                  const SizedBox(height: 28),
+                  AppBar(
+                    backgroundColor: Colors.white,
+                    title: const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Profile',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Billabong',
+                          fontSize: 35,
+                        ),
+                      ),
+                    ),
+
+                    // Bouton de déconnexion
+                    actions: [
+                      IconButton(
+                        icon: const Icon(Icons.logout, color: Colors.black),
+                        onPressed: () async {
+                          await FirebaseAuth.instance.signOut();
+                          // ignore: use_build_context_synchronously
+                          Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(
+                              builder: (context) => const LoginScreen(),
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                    elevation: 0,
+                  ),
+                ],
+              ),
+            ),
+
+            // Affichage des données de l'utilisateur
+            body: ListView(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+
+                      // Affichage de la photo de profil, du nom et de la bio de l'utilisateur
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              CircleAvatar(
+                                backgroundColor: Colors.grey,
+                                backgroundImage: NetworkImage(userData['photoUrl']),
+                                radius: 40,
+                              ),
+                              const SizedBox(height: 10),
+                              Text(
+                                userData['username'],
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                ),
+                              ),
+                              const SizedBox(height: 25),
+                              Text(
+                                userData['bio'],
+                                style: const TextStyle(
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ],
+                          ),
+
+                          // Affichage des statistiques de l'utilisateur
+                          Expanded(
+                            child: Column(
+                              children: [
+                                const SizedBox(height: 20),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    buildStatColumn(postLen, "posts"),
+                                    buildStatColumn(0, "followers"),
+                                    buildStatColumn(0, "following"),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                const Divider(),
+
+                // Affichage des posts de l'utilisateur
+                FutureBuilder(
+                  future: FirebaseFirestore.instance
+                      .collection('posts')
+                      .where('uid', isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+                      .get(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+
+                    return GridView.builder(
+                      shrinkWrap: true,
+                      itemCount: (snapshot.data! as dynamic).docs.length,
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,
+                        crossAxisSpacing: 5,
+                        mainAxisSpacing: 1.5,
+                        childAspectRatio: 1,
+                      ),
+                      itemBuilder: (context, index) {
+                        DocumentSnapshot snap = (snapshot.data! as dynamic).docs[index];
+
+                        // ignore: avoid_unnecessary_containers
+                        return Container(
+                          child: Image(
+                            image: NetworkImage(snap['postUrl']),
+                            fit: BoxFit.cover,
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ],
+            ),
+          );
+  }
+
+  // Fonction qui construit une colonne pour afficher les statistiques
+  Column buildStatColumn(int num, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          num.toString(),
+          style: const TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 4.0,),
+          child: Text(
+            label,
+            style: const TextStyle(
+              fontSize: 15.0,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
