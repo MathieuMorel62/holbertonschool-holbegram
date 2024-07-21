3f7bef0307c7d42d5035633d8545dde8cd2569c2
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-07-21 20:58:04 +0200

    Add profile page UI for edit profile

--- lib/screens/Pages/favorite.dart
+++ lib/screens/Pages/favorite.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-
-// Classe Favorite, affichant la page des favoris de l'utilisateur
+// Widget Favorite, affichant les posts favoris de l'utilisateur
 class Favorite extends StatelessWidget {
   const Favorite({super.key});
 
@@ -11,106 +10,104 @@ class Favorite extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-
-      // Configuration de la barre de navigation
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(90.0),
-        child: Column(
-          children: [
-            const SizedBox(height: 28),
-
-            // Barre de navigation de la page des favoris
-            AppBar(
-              backgroundColor: Colors.white,
-              title: Container(
-                padding: const EdgeInsets.only(bottom: 1.0),
-                child: const Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Favorites',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontFamily: 'Billabong',
-                      fontSize: 35,
-                      fontWeight: FontWeight.w400,
-                    ),
+      body: CustomScrollView(
+        slivers: [
+          // Ajout d'un espace en haut de l'écran
+          const SliverToBoxAdapter(child: SizedBox(height: 28)),
+          // Barre d'application personnalisée
+          SliverAppBar(
+            backgroundColor: Colors.white,
+            elevation: 0,
+            pinned: true,
+            automaticallyImplyLeading: false,
+            title: Container(
+              padding: const EdgeInsets.only(bottom: 1.0),
+              child: const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Favorites',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Billabong',
+                    fontSize: 35,
+                    fontWeight: FontWeight.w400,
                   ),
                 ),
               ),
-              elevation: 0,
             ),
-          ],
-        ),
-      ),
+          ),
+          // Reste de l'écran remplie par le StreamBuilder
+          SliverFillRemaining(
+            child: StreamBuilder(
+              // Écoute des données de la collection 'favorites' de l'utilisateur actuel
+              stream: FirebaseFirestore.instance
+                  .collection('users')
+                  .doc(FirebaseAuth.instance.currentUser!.uid)
+                  .collection('favorites')
+                  .snapshots(),
+              builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                // Gestion des erreurs
+                if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                }
+                // Affichage d'un cercle de chargement si les données sont en cours de chargement
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
 
-      body: StreamBuilder(
-        // Récupération des données de la collection 'favorites' de l'utilisateur
-        stream: FirebaseFirestore.instance
-            .collection('users')
-            .doc(FirebaseAuth.instance.currentUser!.uid)
-            .collection('favorites')
-            .snapshots(),
-        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                // Récupération des documents favoris
+                final favoriteDocs = snapshot.requireData.docs;
 
-          // Gestion des erreurs
-          if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          }
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
+                // Affichage des posts favoris sous forme de liste
+                return ListView.builder(
+                  padding: const EdgeInsets.only(top: 20),
+                  itemCount: favoriteDocs.length,
+                  itemBuilder: (context, index) {
+                    var favoriteDoc = favoriteDocs[index];
 
-          // Récupération des documents des favoris
-          final favoriteDocs = snapshot.requireData.docs;
+                    // FutureBuilder pour récupérer les détails du post favori
+                    return FutureBuilder<DocumentSnapshot>(
+                      future: FirebaseFirestore.instance
+                          .collection('posts')
+                          .doc(favoriteDoc['postId'])
+                          .get(),
+                      builder: (context, AsyncSnapshot<DocumentSnapshot> postSnapshot) {
+                        // Gestion des erreurs
+                        if (postSnapshot.hasError) {
+                          return Center(child: Text('Error: ${postSnapshot.error}'));
+                        }
+                        // Affichage d'un cercle de chargement si les données sont en cours de chargement
+                        if (postSnapshot.connectionState == ConnectionState.waiting) {
+                          return const Center(child: CircularProgressIndicator());
+                        }
 
-          // Affichage des favoris de l'utilisateur sous forme de liste
-          return ListView.builder(
-            itemCount: favoriteDocs.length,
-            itemBuilder: (context, index) {
-              var favoriteDoc = favoriteDocs[index];
+                        // Vérification si le post existe
+                        if (!postSnapshot.hasData || !postSnapshot.data!.exists) {
+                          return Container();
+                        }
 
-              // Utilisation de FutureBuilder pour récupérer les détails des posts favoris
-              return FutureBuilder<DocumentSnapshot>(
-                future: FirebaseFirestore.instance
-                    .collection('posts')
-                    .doc(favoriteDoc['postId'])
-                    .get(),
-                builder: (context, AsyncSnapshot<DocumentSnapshot> postSnapshot) {
+                        // Récupération des données du post
+                        var post = postSnapshot.data!;
 
-                  // Gestion des erreurs lors de la récupération des données
-                  if (postSnapshot.hasError) {
-                    return Center(child: Text('Error: ${postSnapshot.error}'));
-                  }
-                  if (postSnapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  }
-
-                  // Vérification de l'existence des données
-                  if (!postSnapshot.hasData || !postSnapshot.data!.exists) {
-                    return Container();
-                  }
-
-                  // Récupération des données du post
-                  var post = postSnapshot.data!;
-
-                  // Affichage des posts favoris avec une image de couverture
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      height: 250,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: NetworkImage(post['postUrl']),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              );
-            },
-          );
-        },
+                        // Affichage de l'image du post favori
+                        return Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                          height: 250,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: NetworkImage(post['postUrl']),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
