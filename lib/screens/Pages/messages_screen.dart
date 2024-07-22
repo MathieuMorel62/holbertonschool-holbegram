c51f679b772fbda9f56a5e40fd6920807a22ed35
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-07-22 11:33:46 +0200

    Update MessagesScreen UI and logic

--- lib/screens/Pages/messages_screen.dart
+++ lib/screens/Pages/messages_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-// Classe principale de l'écran des messages
 class MessagesScreen extends StatefulWidget {
   const MessagesScreen({super.key});
 
@@ -10,7 +9,6 @@ class MessagesScreen extends StatefulWidget {
   MessagesScreenState createState() => MessagesScreenState();
 }
 
-// État associé à la classe MessagesScreen
 class MessagesScreenState extends State<MessagesScreen> {
   @override
   Widget build(BuildContext context) {
@@ -23,8 +21,8 @@ class MessagesScreenState extends State<MessagesScreen> {
           style: TextStyle(color: Colors.black),
         ),
         elevation: 0,
+        iconTheme: const IconThemeData(color: Colors.black),
       ),
-      // Utilisation de StreamBuilder pour écouter les changements en temps réel dans Firestore
       body: StreamBuilder<QuerySnapshot>(
         stream: FirebaseFirestore.instance
             .collection('users')
@@ -33,19 +31,15 @@ class MessagesScreenState extends State<MessagesScreen> {
             .orderBy('timestamp', descending: true)
             .snapshots(),
         builder: (context, snapshot) {
-          // Affiche un message d'erreur si une erreur se produit
           if (snapshot.hasError) {
             return Center(child: Text('Error: ${snapshot.error}'));
           }
-          // Affiche un indicateur de chargement tant que les données sont en cours de chargement
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
           }
 
-          // Récupère les messages depuis l'instantané de données
           final messages = snapshot.data!.docs;
 
-          // Affiche un message si aucun message n'est disponible
           if (messages.isEmpty) {
             return const Center(
               child: Text(
@@ -55,7 +49,6 @@ class MessagesScreenState extends State<MessagesScreen> {
             );
           }
 
-          // Affiche les messages dans une liste
           return ListView.builder(
             itemCount: messages.length,
             itemBuilder: (context, index) {
@@ -64,17 +57,15 @@ class MessagesScreenState extends State<MessagesScreen> {
             },
           );
         },
-      ),
+      ), // Ajout d'un espace ici
     );
   }
 }
 
-// Classe pour représenter une tuile de message individuelle
 class MessageTile extends StatelessWidget {
   final QueryDocumentSnapshot message;
   const MessageTile({super.key, required this.message});
 
-  // Méthode pour supprimer un message
   void deleteMessage(BuildContext context) async {
     await FirebaseFirestore.instance
         .collection('users')
@@ -83,13 +74,11 @@ class MessageTile extends StatelessWidget {
         .doc(message.id)
         .delete();
 
-    // Affiche une notification de confirmation après la suppression
     ScaffoldMessenger.of(context).showSnackBar(
       const SnackBar(content: Text('Message deleted')),
     );
   }
 
-  // Méthode pour répondre à un message
   void replyToMessage(BuildContext context, String senderId) async {
     TextEditingController replyController = TextEditingController();
 
@@ -123,7 +112,6 @@ class MessageTile extends StatelessWidget {
                   'read': false,
                 });
 
-                // Affiche une notification de confirmation après l'envoi de la réponse
                 ScaffoldMessenger.of(context).showSnackBar(
                   const SnackBar(content: Text('Reply sent successfully')),
                 );
@@ -141,9 +129,11 @@ class MessageTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<DocumentSnapshot>(
-      future: FirebaseFirestore.instance.collection('users').doc(message['senderId']).get(),
+      future: FirebaseFirestore.instance
+          .collection('users')
+          .doc(message['senderId'])
+          .get(),
       builder: (context, snapshot) {
-        // Affiche un nom et un statut de chargement par défaut tant que les données ne sont pas chargées
         if (!snapshot.hasData) {
           return const ListTile(
             title: Text('Anonymous'),
@@ -155,7 +145,6 @@ class MessageTile extends StatelessWidget {
         var senderName = sender['username'];
         var senderPhotoUrl = sender['photoUrl'];
 
-        // Met à jour l'état du message en "lu" si nécessaire
         if (message['read'] == false) {
           FirebaseFirestore.instance
               .collection('users')
@@ -165,25 +154,34 @@ class MessageTile extends StatelessWidget {
               .update({'read': true});
         }
 
-        // Affiche les détails du message dans une tuile
-        return ListTile(
-          leading: CircleAvatar(
-            backgroundImage: NetworkImage(senderPhotoUrl),
-          ),
-          title: Text(senderName),
-          subtitle: Text(message['content']),
-          trailing: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              IconButton(
-                icon: const Icon(Icons.reply, color: Colors.blue),
-                onPressed: () => replyToMessage(context, message['senderId']),
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 45.0),
+          child: Card(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            elevation: 5,
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(senderPhotoUrl),
               ),
-              IconButton(
-                icon: const Icon(Icons.delete, color: Colors.red),
-                onPressed: () => deleteMessage(context),
+              title: Text(senderName,
+                  style: const TextStyle(fontWeight: FontWeight.bold)),
+              subtitle: Text(message['content']),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.reply, color: Colors.blue),
+                    onPressed: () =>
+                        replyToMessage(context, message['senderId']),
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.delete, color: Colors.red),
+                    onPressed: () => deleteMessage(context),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         );
       },
