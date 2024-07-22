85e12076ca85933cd6d32191c52044bf5bd80aa9
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-07-22 11:42:54 +0200

    Add comment for message screen

--- lib/screens/Pages/messages_screen.dart
+++ lib/screens/Pages/messages_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+// Classe principale pour l'écran des messages
 class MessagesScreen extends StatefulWidget {
   const MessagesScreen({super.key});
 
@@ -9,6 +10,7 @@ class MessagesScreen extends StatefulWidget {
   MessagesScreenState createState() => MessagesScreenState();
 }
 
+// État de l'écran des messages
 class MessagesScreenState extends State<MessagesScreen> {
   @override
   Widget build(BuildContext context) {
@@ -24,6 +26,7 @@ class MessagesScreenState extends State<MessagesScreen> {
         iconTheme: const IconThemeData(color: Colors.black),
       ),
       body: StreamBuilder<QuerySnapshot>(
+        // StreamBuilder pour afficher les messages de l'utilisateur
         stream: FirebaseFirestore.instance
             .collection('users')
             .doc(FirebaseAuth.instance.currentUser!.uid)
@@ -38,8 +41,10 @@ class MessagesScreenState extends State<MessagesScreen> {
             return const Center(child: CircularProgressIndicator());
           }
 
+          // Récupérer les messages de l'utilisateur
           final messages = snapshot.data!.docs;
 
+          // Si l'utilisateur n'a pas de messages à afficher
           if (messages.isEmpty) {
             return const Center(
               child: Text(
@@ -49,6 +54,7 @@ class MessagesScreenState extends State<MessagesScreen> {
             );
           }
 
+          // Afficher les messages de l'utilisateur
           return ListView.builder(
             itemCount: messages.length,
             itemBuilder: (context, index) {
@@ -57,11 +63,12 @@ class MessagesScreenState extends State<MessagesScreen> {
             },
           );
         },
-      ), // Ajout d'un espace ici
+      ),
     );
   }
 }
 
+// Classe pour afficher un message
 class MessageTile extends StatelessWidget {
   final QueryDocumentSnapshot message;
   const MessageTile({super.key, required this.message});
@@ -74,14 +81,17 @@ class MessageTile extends StatelessWidget {
         .doc(message.id)
         .delete();
 
+    // Afficher un message de confirmation
     ScaffoldMessenger.of(context).showSnackBar(
       const SnackBar(content: Text('Message deleted')),
     );
   }
 
+  // Fonction pour répondre à un message
   void replyToMessage(BuildContext context, String senderId) async {
     TextEditingController replyController = TextEditingController();
 
+    // Afficher une boîte de dialogue pour répondre au message
     showDialog(
       context: context,
       builder: (context) => AlertDialog(
@@ -112,10 +122,12 @@ class MessageTile extends StatelessWidget {
                   'read': false,
                 });
 
+                // Afficher un message de confirmation
                 ScaffoldMessenger.of(context).showSnackBar(
                   const SnackBar(content: Text('Reply sent successfully')),
                 );
 
+                // Fermer la boîte de dialogue
                 Navigator.of(context).pop();
               }
             },
@@ -126,6 +138,7 @@ class MessageTile extends StatelessWidget {
     );
   }
 
+  // Afficher les détails du message
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<DocumentSnapshot>(
@@ -141,6 +154,7 @@ class MessageTile extends StatelessWidget {
           );
         }
 
+        // Récupérer les détails de l'expéditeur
         var sender = snapshot.data!.data() as Map<String, dynamic>;
         var senderName = sender['username'];
         var senderPhotoUrl = sender['photoUrl'];
@@ -154,6 +168,7 @@ class MessageTile extends StatelessWidget {
               .update({'read': true});
         }
 
+        // Afficher le message de l'expéditeur avec des options pour répondre et supprimer
         return Padding(
           padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 45.0),
           child: Card(
